45eee4ee802f9718f3286c120681f86a1cfede44
Jerry <jerry@Jerry-PC>
2022-10-03 02:19:08 -0500

    v7.0-beta1

--- db.py
+++ db.py
@@ -1,13 +1,15 @@
 import sqlite3
-from datetime import datetime, timezone
 
-# encapsulate logic????
 
 class GmBotDb:
-    logCommandQuery = "INSERT INTO Log (discord_user_id, server_id, discord_name, command, timestamp) VALUES(?, ?, ?, ?, ?)"
+    createStreakTableQuery = "CREATE TABLE IF NOT EXISTS streak(id TEXT PRIMARY KEY, discord_user_id TEXT, discord_username TEXT, discord_server_id TEXT, discord_server_name TEXT, current_count INTEGER, last_gm_on TEXT, longest_streak INTEGER, last_longest_streak_on TEXT) STRICT"
+    getStreakQuery = "SELECT current_count, last_gm_on, longest_streak, last_longest_streak_on, discord_username, discord_server_name FROM streak WHERE id = ?"
+    newUserQuery = "INSERT INTO streak VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    updateUserQuery = "UPDATE streak SET current_count = ?, last_gm_on = ?, longest_streak = ?, last_longest_streak_on = ?, discord_username = ?, discord_server_name = ? WHERE id = ?"
+    topUsersQuery = "SELECT discord_username, current_count, longest_streak FROM streak WHERE discord_server_id = ? ORDER BY longest_streak LIMIT ?"
 
-    def __init__(self, db_filename: str):
-        self.db_connection = sqlite3.connect(db_filename or "gm-bot.db")
+    def __init__(self, db_filename: str = "gm-bot.db"):
+        self.db_connection = sqlite3.connect(db_filename)
         self.db_connection.isolation_level = None
         self.cursor = self.db_connection.cursor()
         self.create_tables()
@@ -16,50 +18,18 @@ class GmBotDb:
         self.db_connection.close()
 
     def create_tables(self):
-        try:
-            self.cursor.execute("PRAGMA foreign_keys = 1")
-            self.cursor.execute("BEGIN")
-            self.cursor.execute(
-                "CREATE TABLE IF NOT EXISTS server(id INTEGER PRIMARY KEY, name VARCHAR, timezone STRING) STRICT"
-            )
-            self.cursor.execute(
-                "CREATE TABLE IF NOT EXISTS streak(id INTEGER PRIMARY KEY, discord_user_id VARCHAR, server_id VARCHAR, last_gm_on INTEGER, last_longest_streak_on STRING, current_streak INTEGER, longest_streak STRING) STRICT"
-            )
-            self.cursor.execute(
-                "CREATE TABLE IF NOT EXISTS log(id INTEGER PRIMARY KEY, discord_user_id VARCHAR, server_id VARCHAR, discord_name VARCHAR, command VARCHAR, timestamp STRING) STRICT"
-            )
-            self.cursor.execute("COMMIT")
-        except:
-            self.cursor.execute("ROLLBACK")
+        self.cursor.execute("PRAGMA foreign_keys = 1")
+        self.cursor.execute(GmBotDb.createStreakTableQuery)
 
-    def gmUser(self, update_user_id: str, server_id: str, activating_user_id: str = "", activating_user_discord_name: str= ""):
-        self.cursor.execute("SELECT name FROM streak WHERE ")
-        try:
-            self.cursor.execute("BEGIN")
-            self.cursor.execute(GmBotDb.logCommandQuery, (activating_user_id, server_id, activating_user_discord_name, "gm", datetime.now(timezone.utc)))
-            self.cursor.execute("COMMIT")
-        except:
-            self.cursor.execute("ROLLBACK")
+    def getStreak(self, key: int):
+        return self.cursor.execute(GmBotDb.getStreakQuery, (key,)).fetchone()
 
-    def getUserStats(
-        self, update_user_id: str, server_id: str, activating_user_id: str = ""
-    ):
-        print()
+    def newUser(self, data):
+        self.cursor.execute(GmBotDb.newUserQuery, data)
 
-    def getLeaderoard(
-        self, limit: int = 10, activating_user_id: str = "", server_id: str = ""
-    ):
-        print()
+    def updateUser(self, data):
+        self.cursor.execute(GmBotDb.updateUserQuery, data)
+    
+    def getTopUsers(self, discord_server_id: str, limit: int = 10):
+        return self.cursor.execute(GmBotDb.topUsersQuery, (discord_server_id, limit))
 
-    def getCurrent(
-        self, limit: int = 10, activating_user_id: str = "", server_id: str = ""
-    ):
-        print()
-
-    def forceOverwrite(
-        self,
-        update_user_id: str = "",
-        activating_user_id: str = "",
-        server_id: str = "",
-    ):
-        print()
