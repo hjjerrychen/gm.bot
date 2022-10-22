3df465a26adcd962f5320837cfb8e881bd6e7d0f
Jerry Chen <chenhongjin2005@hotmail.com>
2022-10-22 01:49:29 -0500

    v7.0-beta3 Records count in addition to streaks

--- db.py
+++ db.py
@@ -2,11 +2,12 @@ import sqlite3
 
 
 class GmBotDb:
-    createStreakTableQuery = "CREATE TABLE IF NOT EXISTS streak(id TEXT PRIMARY KEY, discord_user_id TEXT, discord_username TEXT, discord_server_id TEXT, discord_server_name TEXT, current_count INTEGER, last_gm_on TEXT, longest_streak INTEGER, last_longest_streak_on TEXT)"
-    getStreakQuery = "SELECT current_count, last_gm_on, longest_streak, last_longest_streak_on, discord_username, discord_server_name FROM streak WHERE id = ?"
-    newUserQuery = "INSERT INTO streak VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
-    updateUserQuery = "UPDATE streak SET current_count = ?, last_gm_on = ?, longest_streak = ?, last_longest_streak_on = ?, discord_username = ?, discord_server_name = ? WHERE id = ?"
-    topUsersQuery = "SELECT discord_username, current_count, longest_streak FROM streak WHERE discord_server_id = ? ORDER BY longest_streak LIMIT ?"
+    # current _count => count; current_streak OK, highest_streak/longest_streak
+    createStreakTableQuery = "CREATE TABLE IF NOT EXISTS streak(id TEXT PRIMARY KEY, discord_user_id TEXT, discord_username TEXT, discord_server_id TEXT, discord_server_name TEXT, count INTEGER, last_gm_on TEXT, longest_streak INTEGER, last_longest_streak_on TEXT, current_streak INTEGER, first_gm_on TEXT)"
+    getStreakQuery = "SELECT count, last_gm_on, longest_streak, last_longest_streak_on, discord_username, discord_server_name, first_gm_on, current_streak FROM streak WHERE id = ?"
+    newUserQuery = "INSERT INTO streak VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    updateUserQuery = "UPDATE streak SET count = ?, last_gm_on = ?, longest_streak = ?, last_longest_streak_on = ?, discord_username = ?, discord_server_name = ?, current_streak = ? WHERE id = ?"
+    topUsersQuery = "SELECT discord_username, count, current_streak, longest_streak FROM streak WHERE discord_server_id = ? ORDER BY count DESC LIMIT ?"
 
     def __init__(self, db_filename: str = "gm-bot.db"):
         self.db_connection = sqlite3.connect(db_filename)
